f6987ddd44ef663af3da58788cd2fe0a5fb85e9f
jinkyung22 <wlsrud7192@gmail.com>
2023-07-06 18:13:06 +0900

    수정중

--- Projects/Typescript/TodoList/sample.js
+++
@@ -1,8 +0,0 @@
-var sampleControl = Widget.button({
-  label: "샘플",
-  onClick: function () {
-    alert("샘플입니다");
-  },
-});
-
-document.body.append(sampleControl.el);
---
+++ Projects/Typescript/TodoList/widget/button.ts
@@ -0,0 +1,7 @@
+import { widget } from "./baseWidget.js";
+import { removeControl } from "./core.js";
+
+class Button {
+  id: string;
+  option: object;
+}
